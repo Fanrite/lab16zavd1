d08af185224f98ebd673452510debd25a417856b
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-16 23:10:07 +0200

    Добавьте файлы проекта.

---
+++ lab16zavd1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab16zavd1
+{
+    class Program
+    {
+        struct PIP
+        {
+            public string fname;
+            public string sname;
+            public string father;
+            public void Info()
+            {
+                Console.WriteLine($"Імя: {fname}  Фамілія: {sname}  Побатькові: {father}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PIP pip;
+            pip.fname = "Богдан";
+            pip.sname = "Стицун";
+            pip.father = "Андрійович";
+            pip.Info();
+            Console.ReadLine();
+        }
+    }
+}
